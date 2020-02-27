2d57c2ca1f3189af6ab075ac7712b1e4a1674e87
ekater1na <ekaterina_l@tut.by>
2020-02-27 16:20:26 +0300

    feat: update services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,9 +11,10 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
+
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -20,6 +22,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
 }
 
 getHeroes(): void {
